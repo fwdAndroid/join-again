9eb860e874149562b3f23d581f7f741f04d2a6de
fawad kaleem <fwdkaleem@gmail.com>
2023-06-30 19:42:13 +0500

    asd

--- lib/event_tab/accepted_request.dart
+++ lib/event_tab/accepted_request.dart
@@ -32,80 +32,85 @@ class _AcceptedRequestState extends State<AcceptedRequest> {
             Map<String, dynamic> data =
                 document.data()! as Map<String, dynamic>;
 
-            return Container(
-              margin: EdgeInsets.only(left: 10, right: 10, top: 8),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(0, 0),
-                        blurRadius: 0.5)
-                  ]),
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Event Title: ",
-                              style: TextStyle(
-                                  fontFamily: "ProximaNova",
-                                  fontSize: 16,
-                                  color: Color(0xff160F29),
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(data['title']),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Joiner Name:",
-                              style: TextStyle(
-                                  fontFamily: "ProximaNova",
-                                  fontSize: 16,
-                                  color: Color(0xff160F29),
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(data['joinname']),
-                          ],
-                        ),
-                      ],
+            return data['joinid'] == FirebaseAuth.instance.currentUser!.uid ||
+                    data['creatorid'] == FirebaseAuth.instance.currentUser!.uid
+                ? Container(
+                    margin: EdgeInsets.only(left: 10, right: 10, top: 8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(0, 0),
+                              blurRadius: 0.5)
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    "Event Title: ",
+                                    style: TextStyle(
+                                        fontFamily: "ProximaNova",
+                                        fontSize: 16,
+                                        color: Color(0xff160F29),
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(data['title']),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Joiner Name:",
+                                    style: TextStyle(
+                                        fontFamily: "ProximaNova",
+                                        fontSize: 16,
+                                        color: Color(0xff160F29),
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(data['joinname']),
+                                ],
+                              ),
+                            ],
+                          ),
+                          trailing: data['joinid'] ==
+                                  FirebaseAuth.instance.currentUser!.uid
+                              ? Text("")
+                              : TextButton(
+                                  onPressed: () {
+                                    FirebaseFirestore.instance
+                                        .collection("joins")
+                                        .doc(document.id)
+                                        .update({
+                                      "joinedRequest": "denied",
+                                      "number of joins":
+                                          data["number of joins"] - 1
+                                    }).then((value) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                              content: Text(
+                                                  "User Request is denied to join the event")));
+                                    });
+                                  },
+                                  child: Text(
+                                    "Denied",
+                                    style: TextStyle(color: Colors.red),
+                                  ))),
                     ),
-                    trailing: data['joinid'] ==
-                            FirebaseAuth.instance.currentUser!.uid
-                        ? Text("")
-                        : TextButton(
-                            onPressed: () {
-                              FirebaseFirestore.instance
-                                  .collection("joins")
-                                  .doc(document.id)
-                                  .update({
-                                "joinedRequest": "denied",
-                                "number of joins": data["number of joins"] - 1
-                              }).then((value) {
-                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                    content: Text(
-                                        "User Request is denied to join the event")));
-                              });
-                            },
-                            child: Text(
-                              "Denied",
-                              style: TextStyle(color: Colors.red),
-                            ))),
-              ),
-            );
+                  )
+                : Container();
           }).toList(),
         );
       },
--- lib/event_tab/all_request.dart
+++ lib/event_tab/all_request.dart
@@ -38,107 +38,110 @@ class _AllRequestState extends State<AllRequest> {
             Map<String, dynamic> data =
                 document.data()! as Map<String, dynamic>;
 
-            return Container(
-              margin: EdgeInsets.only(left: 10, right: 10, top: 8),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(0, 0),
-                        blurRadius: 0.5)
-                  ]),
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: ListTile(
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            "Event Title: ",
-                            style: TextStyle(
-                                fontFamily: "ProximaNova",
-                                fontSize: 16,
-                                color: Color(0xff160F29),
-                                fontWeight: FontWeight.w600),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(data['title']),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "Joiner Name:",
-                            style: TextStyle(
-                                fontFamily: "ProximaNova",
-                                fontSize: 16,
-                                color: Color(0xff160F29),
-                                fontWeight: FontWeight.w600),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(data['joinname']),
-                        ],
-                      ),
-                    ],
-                  ),
-                  subtitle:
-                      data['joinid'] == FirebaseAuth.instance.currentUser!.uid
-                          ? Text("")
-                          : Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
+            return data['joinid'] == FirebaseAuth.instance.currentUser!.uid ||
+                    data['creatorid'] == FirebaseAuth.instance.currentUser!.uid
+                ? Container(
+                    margin: EdgeInsets.only(left: 10, right: 10, top: 8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(0, 0),
+                              blurRadius: 0.5)
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ListTile(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
                               children: [
-                                TextButton(
-                                    onPressed: () async {
-                                      FirebaseFirestore.instance
-                                          .collection("joins")
-                                          .doc(document.id)
-                                          .update({
-                                        "joinedRequest": "accepted",
-                                        "number of joins":
-                                            data["number of joins"] + 1
-                                      }).then((value) {
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(SnackBar(
-                                                content: Text(
-                                                    "User Request is accepted to join the event")));
-                                      });
-                                    },
-                                    child: Text(
-                                      "Accept",
-                                      style: TextStyle(color: Colors.green),
-                                    )),
-                                TextButton(
-                                    onPressed: () {
-                                      FirebaseFirestore.instance
-                                          .collection("joins")
-                                          .doc(document.id)
-                                          .update({
-                                        "joinedRequest": "denied",
-                                        "number of joins": accept--
-                                      }).then((value) {
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(SnackBar(
-                                                content: Text(
-                                                    "User Request is denied to join the event")));
-                                      });
-                                    },
-                                    child: Text(
-                                      "Denied",
-                                      style: TextStyle(color: Colors.red),
-                                    ))
+                                Text(
+                                  "Event Title: ",
+                                  style: TextStyle(
+                                      fontFamily: "ProximaNova",
+                                      fontSize: 16,
+                                      color: Color(0xff160F29),
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(data['title']),
                               ],
                             ),
-                ),
-              ),
-            );
+                            Row(
+                              children: [
+                                Text(
+                                  "Joiner Name:",
+                                  style: TextStyle(
+                                      fontFamily: "ProximaNova",
+                                      fontSize: 16,
+                                      color: Color(0xff160F29),
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(data['joinname']),
+                              ],
+                            ),
+                          ],
+                        ),
+                        subtitle: data['joinid'] ==
+                                FirebaseAuth.instance.currentUser!.uid
+                            ? Text("")
+                            : Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextButton(
+                                      onPressed: () async {
+                                        FirebaseFirestore.instance
+                                            .collection("joins")
+                                            .doc(document.id)
+                                            .update({
+                                          "joinedRequest": "accepted",
+                                          "number of joins":
+                                              data["number of joins"] + 1
+                                        }).then((value) {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(SnackBar(
+                                                  content: Text(
+                                                      "User Request is accepted to join the event")));
+                                        });
+                                      },
+                                      child: Text(
+                                        "Accept",
+                                        style: TextStyle(color: Colors.green),
+                                      )),
+                                  TextButton(
+                                      onPressed: () {
+                                        FirebaseFirestore.instance
+                                            .collection("joins")
+                                            .doc(document.id)
+                                            .update({
+                                          "joinedRequest": "denied",
+                                          "number of joins": accept--
+                                        }).then((value) {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(SnackBar(
+                                                  content: Text(
+                                                      "User Request is denied to join the event")));
+                                        });
+                                      },
+                                      child: Text(
+                                        "Denied",
+                                        style: TextStyle(color: Colors.red),
+                                      ))
+                                ],
+                              ),
+                      ),
+                    ),
+                  )
+                : Container();
           }).toList(),
         );
       },
