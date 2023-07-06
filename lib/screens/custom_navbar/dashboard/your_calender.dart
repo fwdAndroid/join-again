623162be3c25e16a3e1c2cc20be5149528dbf25b
fawad kaleem <fwdkaleem@gmail.com>
2023-07-06 05:16:31 +0500

    Remove Extra Calender Page

--- lib/screens/custom_navbar/dashboard/your_calender.dart
+++ lib/screens/custom_navbar/dashboard/your_calender.dart
@@ -1,47 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:join/screens/calender/all.dart';
 import 'package:join/screens/calender/ongoing.dart';
-import 'package:join/screens/calender/past.dart';
 
 class YourCalender extends StatelessWidget {
   const YourCalender({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 3,
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           centerTitle: true,
           automaticallyImplyLeading: false,
           title: const Text(
             "Your Calender",
-            style: TextStyle(fontFamily: "ProximaNova", fontSize: 20, color: Color(0xff160F29), fontWeight: FontWeight.w600),
-          ),
-          bottom: const TabBar(
-            labelColor: Colors.black,
-            tabs: <Widget>[
-              Tab(
-                text: "All",
-              ),
-              Tab(
-                text: "OnGoing",
-              ),
-              Tab(
-                text: 'Past',
-              ),
-            ],
+            style: TextStyle(
+                fontFamily: "ProximaNova",
+                fontSize: 20,
+                color: Color(0xff160F29),
+                fontWeight: FontWeight.w600),
           ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            All(),
-            OnGoing(),
-            Past(),
-          ],
-        ),
-      ),
-    );
+        body: OnGoing());
   }
 }
